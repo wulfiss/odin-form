458008877e63c0030d1d43681a49694ebf84572b
wulfiss <96155980+wulfiss@users.noreply.github.com>
2022-07-15 05:59:45 -0300

    change event focus for keyup in pass comprobation

--- src/form.js
+++ src/form.js
@@ -3,7 +3,7 @@ let passTwo = document.querySelector('#psw2');
 let spanPass = document.querySelector('.matchPass');
 let $submit = document.querySelector('.btnSubmit');
 
-passTwo.addEventListener('focusout', (event) =>{
+passTwo.addEventListener('keyup', (event) =>{
     if (passOne.value === passTwo.value){
         passTwo.setAttribute('style', 'border: 1px solid rgb(7, 236, 7)');
         spanPass.setAttribute('style', 'display: none');
